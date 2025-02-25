e260cba737fd527f0c055142f1704b965739bb14
dev@steiiin.de <dev@steiiin.de>
2025-02-25 21:05:56 +0100

    style: improve readability of NsDosageUsage

--- src/components/NsDosageUsage.vue
+++ src/components/NsDosageUsage.vue
@@ -1,5 +1,5 @@
 <template>
-  <ns-content-split class="ns-dosage-usage" :class="{ 'nodivider': nodivider  }">
+  <ns-content-split class="ns-dosage-usage" :class="{ nodivider }">
     <template #left>
       <span class="usage-divider" :class="usageColor">
         <span>{{ usageTag }}</span>
@@ -14,51 +14,45 @@
 </template>
 
 <script setup lang="ts">
-import NsContentSplit from '@/components/NsContentSplit.vue'
+
 import { computed } from 'vue'
+import NsContentSplit from '@/components/NsContentSplit.vue'
+
 const props = defineProps<{
   type: "iv" | "im" | "nasal" | "pi" | "supp" | "none",
   label?: string,
   nodivider?: boolean,
 }>()
-const usageTag = computed(() => {
-  if (props.label) { return props.label}
-  switch (props.type) {
-    case "iv":
-      return 'intravenös'
-    case "im":
-      return 'intramuskulär'
-    case "nasal":
-      return 'intranasal'
-    case "pi":
-      return 'inhalativ'
-    case "supp":
-      return 'rektal'
-    default:
-      return ' '
-  }
-})
-const usageColor = computed(() => {
-  switch (props.type) {
-    case "iv":
-      return 'red'
-    case "im":
-      return 'blue'
-    case "nasal":
-      return 'green'
-    case "pi":
-      return 'blue'
-    case "supp":
-      return 'orange'
-    default:
-      return 'none'
-  }
-})
+
+const usageTagMap: Record<string, string> = {
+  iv: 'intravenös',
+  im: 'intramuskulär',
+  nasal: 'nasal',
+  pi: 'inhalativ',
+  supp: 'rektal',
+  none: ' ',
+}
+const usageTag = computed(() => props.label || (props.type && props.type in usageTagMap ? usageTagMap[props.type] : null))
+
+const usageColorMap: Record<string, string> = {
+  iv: 'red',
+  im: 'blue',
+  nasal: 'green',
+  pi: 'blue',
+  supp: 'orange',
+  none: 'none',
+}
+const usageColor = computed(() => props.type && props.type in usageColorMap ? usageColorMap[props.type] : 'none')
+
 </script>
 
 <style lang="css" scoped>
-.usage-divider
-{
+
+.ns-dosage-usage {
+  margin-bottom: var(--ns-card-padding);
+}
+
+.ns-dosage-usage .usage-divider {
   display: flex;
   position: absolute;
   top: 0;
@@ -69,60 +63,60 @@ const usageColor = computed(() => {
   letter-spacing: 1px;
   border-right: 3px solid;
 }
-.ns-dosage-usage.nodivider .usage-divider
-{
+.ns-dosage-usage.nodivider .usage-divider {
   border-right: none;
 }
-.usage-divider span
-{
+
+.usage-divider span {
   margin: auto 0;
   writing-mode: vertical-lr;
 }
 
-.usage-divider.red { border-color: rgba(var(--ns-color-red-rgb), .2) }
-.usage-divider.blue { border-color: rgba(var(--ns-color-blue-rgb), .2) }
-.usage-divider.green { border-color: rgba(var(--ns-color-green-rgb), .2) }
-.usage-divider.orange { border-color: rgba(var(--ns-color-orange-rgb), .2) }
-.usage-divider.none { border-color: transparent }
-
-.ns-dosage-usage
-{
-  margin-bottom: var(--ns-card-padding);
+.usage-divider.red {
+  border-color: rgba(var(--ns-color-red-rgb), .4)
 }
-.usage-content
-{
+.usage-divider.blue {
+  border-color: rgba(var(--ns-color-blue-rgb), .4)
+}
+.usage-divider.green {
+  border-color: rgba(var(--ns-color-green-rgb), .4)
+}
+.usage-divider.orange {
+  border-color: rgba(var(--ns-color-orange-rgb), .4)
+}
+.usage-divider.none {
+  border-color: transparent
+}
+
+.usage-content {
   display: flex;
   flex-direction: column;
   gap: .5rem;
 }
 
 /* text styles */
-.usage-content :deep(hr)
-{
+.usage-content :deep(hr) {
   margin: 0px var(--ns-card-neg-padding) 0 -2px;
   padding: 0 0 0 0;
   border-bottom: 1px solid var(--ion-color-light);
 }
-.usage-content :deep(*)
-{
-  margin: 0; padding: 0;
+.usage-content :deep(*) {
+  margin: 0;
+  padding: 0;
 }
-.usage-content :deep(h2)
-{
+.usage-content :deep(h2) {
   font-size: 1em;
   font-weight: bold;
   margin-bottom: calc(-1 * var(--ns-text-padding));
 }
-.usage-content :deep(h2:last-child)
-{
+.usage-content :deep(h2:last-child) {
   margin-bottom: 0;
 }
-.usage-content :deep(pre)
-{
+.usage-content :deep(pre) {
   display: inline;
 }
-.usage-content :deep(p)
-{
+.usage-content :deep(p) {
   line-height: var(--ns-narrow-line);
 }
+
 </style>
