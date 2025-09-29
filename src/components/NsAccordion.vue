65e038fbfa4140173c589ca5f54dee855a42a207
dev@steiiin.de <dev@steiiin.de>
2025-09-29 14:49:10 +0200

    feat: add accordion component

---
+++ src/components/NsAccordion.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-accordion :value="id ? id : title.toLowerCase()" :class="{ 'with-gap': gap }">
+    <ion-item slot="header" :color="color ? color : 'light'">
+      <ion-label>{{ title }}</ion-label>
+    </ion-item>
+    <div id="ns-content-bg" slot="content">
+      <slot></slot>
+    </div>
+  </ion-accordion>
+</template>
+
+<script setup lang="ts">
+import { IonAccordion, IonItem, IonLabel } from '@ionic/vue'
+defineProps<{
+  title: string,
+  id?: string,
+  color?: string,
+  gap?: boolean
+}>()
+</script>
+
+<style lang="css" scoped>
+
+.with-gap {
+  margin-top: 4px;
+}
+
+</style>
---
+++ src/components/NsAccordionGroup.vue
@@ -0,0 +1,13 @@
+<template>
+  <ion-accordion-group :animated="false">
+    <slot></slot>
+  </ion-accordion-group>
+</template>
+
+<script setup lang="ts">
+import { IonAccordionGroup } from '@ionic/vue'
+</script>
+
+<style lang="css" scoped>
+
+</style>
