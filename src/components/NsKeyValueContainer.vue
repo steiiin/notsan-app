a3a5baa43700c4bec64cee79a33decbd3ef57079
steiiin <52369133+steiiin@users.noreply.github.com>
2025-09-29 16:01:06 +0200

    Add NsKeyValue components

---
+++ src/components/NsKeyValueContainer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="ns-key-value-container">
+    <div class="ns-key-value-table">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.ns-key-value-table {
+  display: table;
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0;
+}
+
+:deep(.ns-key-value) {
+  display: table-row;
+}
+
+:deep(.ns-key-value__cell) {
+  display: table-cell;
+  vertical-align: top;
+  padding: 0.25rem 0;
+}
+
+:deep(.ns-key-value__cell--key) {
+  width: 50%;
+  max-width: 50%;
+  padding-right: 1.5rem;
+}
+
+:deep(.ns-key-value__cell--value) {
+  width: auto;
+}
+</style>
