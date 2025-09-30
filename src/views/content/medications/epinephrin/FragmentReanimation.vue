472da4db54505e181fec81972a3ba3f6422786d8
dev@steiiin.de <dev@steiiin.de>
2025-09-30 11:15:23 +0200

    feat: add adrenalin reanimation fragment (stub)

---
+++ src/views/content/medications/epinephrin/FragmentReanimation.vue
@@ -0,0 +1,25 @@
+<template>
+  <ns-content-group title="Adrenalin" dense>
+  </ns-content-group>
+</template>
+
+<script setup lang="ts">
+
+import NsContentGroup from '@/components/NsContentGroup.vue';
+import NsContentSplit from '@/components/NsContentSplit.vue';
+import NsTextContent from '@/components/NsTextContent.vue';
+import NsKeyValueContainer from '@/components/NsKeyValueContainer.vue';
+import NsKeyValue from '@/components/NsKeyValue.vue';
+import TextMono from '@/components/TextMono.vue';
+import NsColorBox from '@/components/NsColorBox.vue';
+import TextColored from '@/components/TextColored.vue';
+import { Patient } from '@/types/emergency';
+
+import { clamp, round } from '@/service/math';
+import { computed } from 'vue';
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
