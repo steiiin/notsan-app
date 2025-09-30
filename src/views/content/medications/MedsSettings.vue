817733ddf75aed1c7061d9eceedd8da4dd70648e
steiiin <52369133+steiiin@users.noreply.github.com>
2025-09-30 15:58:19 +0200

    Add medications settings modal

---
+++ src/views/content/medications/MedsSettings.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Medikament-Einstellungen</ion-title>
+        <ion-buttons slot="end">
+          <ion-button fill="clear" @click="close">
+            <ion-icon :icon="closeOutline" slot="icon-only" />
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ion-text color="medium">
+        Weitere Einstellungen werden hier in Zukunft verfügbar sein.
+      </ion-text>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  IonContent,
+  IonText
+} from '@ionic/vue';
+import { closeOutline } from 'ionicons/icons'
+
+const emit = defineEmits<{
+  (e: 'close'): void
+}>()
+
+const close = () => {
+  emit('close')
+}
+</script>
