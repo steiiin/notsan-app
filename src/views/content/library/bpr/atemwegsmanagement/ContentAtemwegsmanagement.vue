f9dbae38743c377a9aff08c7bc1e1433c3a91929
dev@steiiin.de <dev@steiiin.de>
2025-10-02 08:09:28 +0200

    stub: helper content added

--- src/views/content/library/bpr/atemwegsmanagement/ContentAtemwegsmanagement.vue
+++ src/views/content/library/bpr/atemwegsmanagement/ContentAtemwegsmanagement.vue
@@ -1,9 +1,13 @@
 <template>
   <ns-flow @action="handleAction" :svg="flowSvg"></ns-flow>
+  <ns-content-group title="hello">
+    hello
+  </ns-content-group>
 </template>
 
 <script setup lang="ts">
 import NsFlow from '@/components/library/NsFlow.vue'
+import NsContentGroup from '@/components/NsContentGroup.vue'
 import flowSvg from './flow.svg?raw'
 
 function handleAction(key: string) {
