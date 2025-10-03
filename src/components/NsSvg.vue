3426ffb71d2d2bf9ac0ce9c11bf29c1b967d2246
dev@steiiin.de <dev@steiiin.de>
2025-10-03 08:32:52 +0200

    feat: svg viewer

---
+++ src/components/NsSvg.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ns-svg" v-html="svg"></div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ svg: string }>();
+</script>
+
+<style scoped>
+.ns-svg {
+  display: flex;
+}
+
+.ns-svg :deep(svg) {
+  width: 100%;
+  height: auto;
+}
+</style>
