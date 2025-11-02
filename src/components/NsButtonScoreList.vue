0c2679771a0cc4695dfdee1a5c4ab8249a85bd0d
dev@steiiin.de <dev@steiiin.de>
2025-11-02 19:17:02 +0100

    [feature] Form: KOF-Rechner Entscheidung Verbrennungszentrum Implements #125

---
+++ src/components/NsButtonScoreList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ns-button-score-list">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.ns-button-score-list :deep(.ns-button-score-group:nth-child(even)) {
+  background-color: var(--ion-color-light);
+  margin-inline: var(--ns-card-neg-padding);
+  padding-inline: var(--ns-card-padding);
+}
+</style>
--- src/views/content/forms/diagnose/kof/ContentKof.vue
+++ src/views/content/forms/diagnose/kof/ContentKof.vue
@@ -7,12 +7,28 @@
     <div class="subtitle">{{ infoData.subtitle }}</div>
     <div class="percs" v-html="infoData.percs" v-if="infoData.percs"></div>
   </ns-content-group>
-   <ns-content-group style="margin-top:-0.3rem">
+  <ns-content-group style="margin-top:-0.3rem">
     <div class="buttons">
       <ion-button @click="openPaint" color="light" class="block">Bereiche {{ hasAlreadyPaint ? 'bearbeiten' : 'einzeichnen' }}</ion-button>
       <ion-button @click="resetPaint" color="danger" v-if="hasAlreadyPaint">Neu</ion-button>
     </div>
   </ns-content-group>
+  <template v-if="hasAlreadyPaint">
+    <ns-content-group title="Entscheidung Brandverletztenzentrum" v-if="!needBurnclinicByVkof">
+      <ns-button-score-list>
+        <ns-button-score-group v-model="bc_chemical" :option="{
+          type: 'yes-no', label: 'Verätzung durch Chemikalien?'}">
+        </ns-button-score-group>
+        <ns-button-score-group v-model="bc_inhal" :option="{
+          type: 'yes-no', label: 'Inhaltationstrauma?'}">
+        </ns-button-score-group>
+      </ns-button-score-list>
+    </ns-content-group>
+    <ns-content-group :style="burnclinicInfoData.color">
+      <div class="title">{{ burnclinicInfoData.title }}</div>
+      <div class="subtitle">{{ burnclinicInfoData.subtitle }}</div>
+    </ns-content-group>
+  </template>
 
 </template>
 
@@ -21,6 +37,8 @@
 import { IonButton } from '@ionic/vue';
 import KofPaint from '@/components/forms/kof/KofPaint.vue';
 import NsContentGroup from '@/components/NsContentGroup.vue';
+import NsButtonScoreGroup from '@/components/NsButtonScoreGroup.vue';
+import NsButtonScoreList from '@/components/NsButtonScoreList.vue';
 import { KofInfo } from '@/types/forms/kof';
 
 import { computed, ref } from 'vue';
@@ -34,6 +52,8 @@ const openPaint = () => {
 }
 const resetPaint = () => {
   kof.value = { patientAge: 'adult' }
+  bc_chemical.value = false
+  bc_inhal.value = false
   openPaint()
 }
 const hasAlreadyPaint = computed(() => !!kof.value.paintState)
@@ -98,7 +118,6 @@ const cardColorStyle = (colorCode?: string): string => {
     return ''
   }
 }
-
 const percColorStyle = (colorCode?: string): string => {
   if (!!colorCode && colorCode.length>0)
   {
@@ -110,7 +129,6 @@ const percColorStyle = (colorCode?: string): string => {
     return '--ns-kof-perc-color: var(--ion-color-dark-contrast-rgb);'
   }
 }
-
 const percRoundedValue = (perc: number): number => {
   return round(perc, perc>5 ? 5 : 1)
 }
@@ -121,9 +139,54 @@ const degreePercs = (pct1: number, pct2: number, pct3: number) => {
   if (pct1 > 0) { parts.push({ deg: '1°', color: percColorStyle('1st'), val: percRoundedValue(pct1) }) }
   if (pct2 > 0) { parts.push({ deg: '2°', color: percColorStyle('2nd'), val: percRoundedValue(pct2) }) }
   if (pct3 > 0) { parts.push({ deg: '3°', color: percColorStyle('3rd'), val: percRoundedValue(pct3) }) }
-  if (parts.length>1) { parts.push({ deg: '∑', color: percColorStyle(), val: parts.map(e => e.val).reduce((a, v) => a + v, 0) }) }
+  if (parts.length>1) { parts.push({ deg: 'VKOF', color: percColorStyle(), val: parts.map(e => e.deg.startsWith('1') ? 0 : e.val).reduce((a, v) => a + v, 0) }) } // VKOF wird ohne 1° ermittelt
   return parts.map(e => `<code style="${e.color}">${e.deg} <i>${e.val}%</i></code>`).join('')
 }
+
+/** Indikation für stationäre Behandlung in Zentren für Brandverletzte
+ *  (AWMF-S2K-Leitlinie 2021)
+ *  - Verbrennung 2° >= 10% VKOF
+ *  - Verbrennung 3°
+ *  - Verbrennung > 1° an Hand/Genital/Gesicht
+ *  - Chemikalienverätzungen
+ *  - Inhalationstrauma
+ *  - (Begleiterkrankungen, die die Behandlung erschweren)
+ *  - (spezielle psychologische, psychiatrische oder physische Betreuung notwendig)
+ */
+
+const needBurnclinicByVkof = computed(() => bc_vkof10.value || bc_3rd.value || bc_crit.value)
+
+const bc_chemical = ref(false)
+const bc_inhal = ref(false)
+
+const bc_vkof10 = computed(() => kof.value.calculation ? (kof.value.calculation.perc2nd+kof.value.calculation.perc3rd) >= 10 : false)
+const bc_3rd = computed(() => kof.value.calculation?.has3rd ?? false)
+const bc_crit = computed(() => kof.value.calculation ? (kof.value.calculation.criticalRegions.hasFace || kof.value.calculation.criticalRegions.hasGenital || kof.value.calculation.criticalRegions.hasHands) : false)
+
+const burnclinicInfoData = computed(() => {
+
+  const checks = []
+  if (bc_vkof10.value) { checks.push('VKOF≥10%') }
+  if (bc_3rd.value) { checks.push('3°') }
+  if (bc_crit.value) { checks.push(infoData.value.subtitle) }
+  if (bc_chemical.value) { checks.push('Verätzung') }
+  if (bc_inhal.value) { checks.push('Inhalationstrauma') }
+
+
+  if (checks.length == 0) {
+    let subtitle = 'Nur 1° Verbrennungen'
+    if (kof.value.calculation?.has2nd) { subtitle = 'VKOF unter 10%' }
+    return { title: 'Verbrennungsbett nicht notwendig', subtitle, color: cardColorStyle('') }
+  }
+  else
+  {
+    let color = '1st'
+    if (kof.value.calculation?.has3rd) { color = '3rd' }
+    else if (kof.value.calculation?.has2nd) { color = '2nd' }
+    return { title: 'Verbrennungsbett notwendig', subtitle: checks.join(', '), color: cardColorStyle(color) }
+  }
+
+})
 
 </script>
 
@@ -148,7 +211,7 @@ const degreePercs = (pct1: number, pct2: number, pct3: number) => {
 
 .percs {
   display: flex;
-  gap: 0.5rem;
+  gap: 0.3rem;
 }
 
 .percs :deep(code) {
@@ -156,8 +219,8 @@ const degreePercs = (pct1: number, pct2: number, pct3: number) => {
   background-color: rgba(var(--ns-kof-perc-color), 0.3);
   border: 1px solid rgba(var(--ns-kof-perc-color), 0.8);
   border-radius: 4px;
-  padding: .25rem .75rem;
-  gap: .3rem;
+  padding: .1rem .5rem;
+  gap: .2rem;
   font-size: 1.1em;
   color: var(--ion-color-light-contrast);
   font-family: monospace;
@@ -167,7 +230,7 @@ const degreePercs = (pct1: number, pct2: number, pct3: number) => {
 
 .percs :deep(code i)::before {
   content: '|';
-  padding-right: .3rem;
+  padding-right: .2rem;
 }
 
 .percs :deep(code i) {
