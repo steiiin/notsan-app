5f91df8f909b61ed2ef6918c33477d6e0e64fd6d
dev@steiiin.de <dev@steiiin.de>
2026-04-23 17:23:07 +0200

    fixes #31

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,15 +3,15 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="meds" href="/tabs/meds">
+        <ion-tab-button tab="meds" href="/tabs/meds" @click="openMeds">
           <ion-icon aria-hidden="true" :icon="medkit" />
           <ion-label>Medikamente</ion-label>
         </ion-tab-button>
-        <ion-tab-button tab="lib" href="/tabs/lib">
+        <ion-tab-button tab="lib" href="/tabs/lib" @click="openLibrary">
           <ion-icon aria-hidden="true" :icon="library" />
           <ion-label>Wissen</ion-label>
         </ion-tab-button>
-        <ion-tab-button tab="calc" href="/tabs/forms">
+        <ion-tab-button tab="calc" href="/tabs/forms" @click="openForms">
           <ion-icon aria-hidden="true" :icon="calculator" />
           <ion-label>Rechner</ion-label>
         </ion-tab-button>
@@ -25,8 +25,20 @@
 </template>
 <script setup lang="ts">
 
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue'
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet, useIonRouter } from '@ionic/vue'
 import { medkit, library, calculator, warning } from 'ionicons/icons'
+
+const ionRouter = useIonRouter()
+
+const openMeds = () => {
+  ionRouter.navigate('/tabs/meds', 'root', 'replace')
+}
+const openLibrary = () => {
+  ionRouter.navigate('/tabs/lib', 'root', 'replace')
+}
+const openForms = () => {
+  ionRouter.navigate('/tabs/forms', 'root', 'replace')
+}
 
 </script>
 <style lang="css" scoped>
