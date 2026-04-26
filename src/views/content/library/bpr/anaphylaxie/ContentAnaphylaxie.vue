79420a11374f00b7fc5db46ba2f41c7481a11ceb
dev@steiiin.de <dev@steiiin.de>
2026-04-26 15:15:01 +0200

    fix: anaphylaxie flow

--- src/views/content/library/bpr/anaphylaxie/ContentAnaphylaxie.vue
+++ src/views/content/library/bpr/anaphylaxie/ContentAnaphylaxie.vue
@@ -8,24 +8,24 @@
   </ns-content-group>
   <ns-flow :svg="flowSvg" @action="handleAction"></ns-flow>
 
-  <ns-flow-action ref="action_basic">
+  <ns-flow-action ref="action_simple">
     <h2>Allergische Reaktion (I)</h2>
     <p>Leichte Auswirkungen.</p>
     <hr>
-    <ul>
-      <li>Hautreaktionen <i>(z.B. Juckreiz, Quaddeln, Rötung)</i></li>
-      <li>Abdominelle Beschwerden <i>(z.B. Schmerzen, Übelkeit)</i></li>
+    <ul head>
+      <li>Hautreaktionen: <i>Juckreiz, Quaddeln, Rötung</i></li>
+      <li>Abdominelle Beschwerden: <i>Schmerzen, Übelkeit</i></li>
     </ul>
   </ns-flow-action>
   <ns-flow-action ref="action_severe">
     <h2>Anaphylaxie (II+III)</h2>
     <p>Zusätzlich zur Hautreaktion treten ABCD-Probleme auf.</p>
     <hr>
-    <ul>
-      <li><b>A</b> <i>(z.B. Schwellung, Heiserkeit, Stridor)</i></li>
-      <li><b>B</b> <i>(z.B. Tachypnoe, Obstruktion, Zyanose)</i></li>
-      <li><b>C</b> <i>(z.B. Kaltschweißigkeit, Schockig)</i></li>
-      <li><b>D</b> <i>(z.B. Verwirrung, Unruhe, Eintrübung)</i></li>
+    <ul head>
+      <li>A: <i>Schwellung, Heiserkeit, Stridor</i></li>
+      <li>B: <i>Tachypnoe, Obstruktion</i></li>
+      <li>C: <i>Kaltschweißigkeit, Schockig</i></li>
+      <li>D: <i>Verwirrung, Unruhe, Eintrübung</i></li>
     </ul>
   </ns-flow-action>
 
@@ -41,12 +41,12 @@ import flowSvg from './flow.svg?raw'
 import { ref } from 'vue'
 import { FlowActionPayload } from '@/types/flow'
 
-const action_basic = ref<InstanceType<typeof NsFlowAction> | null>(null)
+const action_simple = ref<InstanceType<typeof NsFlowAction> | null>(null)
 const action_severe = ref<InstanceType<typeof NsFlowAction> | null>(null)
 
 function handleAction(payload: FlowActionPayload) {
-  if (payload.key === 'basic') {
-    action_basic.value?.presentPopover(payload)
+  if (payload.key === 'simple') {
+    action_simple.value?.presentPopover(payload)
   }
   else if (payload.key === 'severe') {
     action_severe.value?.presentPopover(payload)
