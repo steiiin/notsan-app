900600692ce7a9660752ed64c32b2bc6f1099c3c
dev@steiiin.de <dev@steiiin.de>
2026-07-28 20:08:10 +0200

    BPR: ACS Fixes #165

--- src/composables/useContentLink.ts
+++ src/composables/useContentLink.ts
@@ -2,6 +2,7 @@
 const fixed_keywords: Record<string, string> = {
   sepsis: '/tabs/forms/list/score/score-sepsis',
   geburtTransport: '/tabs/forms/list/decision/decision-geburt',
+  acs: '/tabs/forms/list/diagnose/diagnose-acs',
 }
 
 export function useContentLink(link: string): string {
---
+++ src/views/content/library/bpr/acs/ContentAcs.vue
@@ -0,0 +1,27 @@
+<template>
+  <ns-content-group>
+    <ns-text-content>
+      <p>
+        Stufenweises Vorgehen zur <b>Akutem Koronarsyndrom</b>.
+      </p>
+    </ns-text-content>
+  </ns-content-group>
+  <ns-flow :svg="flowSvg"></ns-flow>
+</template>
+
+<script setup lang="ts">
+import NsFlow from '@/components/library/NsFlow.vue'
+import NsFlowAction from '@/components/library/NsFlowAction.vue'
+import NsContentGroup from '@/components/NsContentGroup.vue'
+import NsTextContent from '@/components/NsTextContent.vue'
+
+import flowSvg from './flow.svg?raw'
+import { ref } from 'vue'
+import { FlowActionPayload } from '@/types/flow'
+
+</script>
+
+<style scoped>
+
+  /* padding: calc(0.5 * var(--ns-card-padding));  <-- move this elsewhere */
+</style>
